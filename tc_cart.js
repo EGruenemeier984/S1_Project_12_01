64763a7a3c5856299371c29d6db255752f8ac107
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-02-28 13:31:12 -0700

    Final Commit
    Finished adding all comments to the project and now its ready for review.

--- tc_cart.js
+++ tc_cart.js
@@ -11,17 +11,24 @@
    Filename: tc_cart.js
 	
 */
+// This var is set to 0;
 var orderTotal = 0;
-
+// this var is set to a string that is an HTML table. 
 var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
-
+// This is a for loop that starts at 0 and stops when it reaches three while iterating by one evreytime
 for (var i = 0; i <= 3; i++) {
+    // This adds the table data cell with the image of the item they have in there cart
     cartHTML += `<tr> <td><img src='tc_${item[i]}.png' alt='${item[i]}' /></td>`;
+    // this adds another cell row that contains the description for evrey item thats in the cart.
     cartHTML += `<td>${itemDescription[i]}</td> <td>$${itemPrice[i]}</td> <td>${itemQty[i]}</td>`;
+    // this establishes a var and sets it equal to the price of that item and multiplies it by the quantity.
     var itemCost = itemPrice[i] * itemQty[i];
-    cartHTML += `<td>$${itemCost›}</td></tr>`;
-    orderTotal = orderTotal + itemCost;
+    // this adds another data row for the cost of the item 
+    cartHTML += `<td>$${itemCost}</td></tr>`;
+    // this sets the total equal to the total plus the item cost.
+    orderTotal += itemCost;
 }
+// This adds the closing part of the table to the HTML to display the order total.
 cartHTML += `<tr> <td colspan='4'>Subtotal</td> <td>$${orderTotal}</td> </tr> </table>`;
-
+// This sets the cartHTML var to the innerHTML of the ID created in the HTML.
 document.getElementById("cart").innerHTML = cartHTML;
--- tc_order.js
+++ tc_order.js
@@ -16,10 +16,11 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+// This var is an item with 4 numbers in the array
 var item = [10582, 23015, 41807, 10041];
-
+// the item description var is an array full of the descriptions for each item. 
 var itemDescription = ["1975 Green Bay Packers Football (signed), Item 10582", "Tom Landry 1955 Football Card (unsigned), Item 23015", "1916 Army-Navy Game, Framed Photo (signed), Item 41807", "Protective Card Sheets, Item 10041"];
-
+// This item price var sets the price for each item.
 var itemPrice = [149.93, 89.98, 334.93, 22.67];
-
+// this array is set to the quantity of each item they purchase
 var itemQty = [1, 1, 1, 4];
